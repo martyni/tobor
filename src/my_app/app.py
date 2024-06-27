a0dba6d9502c7ecc2ea3f3bf1df68316081fe0ec
Your Name <you@example.com>
2024-06-27 22:09:01 +0100

    .

---
+++ src/my_app/app.py
@@ -0,0 +1,15 @@
+'''
+Default Boilerplate Module
+'''
+
+def main():
+    '''
+    Main funtion for Default Boilerplate Module
+    '''
+    message = 'hi'
+    return message
+
+
+
+if __name__ == '__main__':
+    print(main())
