62d18ee504f387d7dc13f0ed64e36c185e3cac6b
askMartyn <martyn@rustyquill.com>
2026-01-19 17:30:50 +0000

    pipeline

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,8 +4,21 @@ DOCKER_FLAGS=$(cat ${RUN_DIR}/DOCKER_FLAGS)
 DOCKER_REPO=$(cat ${RUN_DIR}/DOCKER_REPO)
 NAME=$(cat ${RUN_DIR}/NAME)
 VERSION=$(cat ${RUN_DIR}/VERSION)
-export TEST_CONTAINER=$(docker run ${DOCKER_FLAGS} --env-file /home/app/env -v ~/.ssh:/home/app/.ssh ${DOCKER_REPO}/${NAME}:${VERSION})
+if [ -f /home/app/env ]; then; 
+    export TEST_CONTAINER=$(docker run ${DOCKER_FLAGS} --env-file /home/app/env -v ~/.ssh:/home/app/.ssh ${DOCKER_REPO}/${NAME}:${VERSION})
+else;
+    cat <<\EOT >env
+MOD_USER_CHANNEL_ID=${MOD_USER_CHANNEL_ID}
+MY_CHANNEL_ID=${MY_CHANNEL_ID}
+TOBOR_ACCESS_TOKEN=${TOBOR_ACCESS_TOKEN}
+TOBOR_CLIENT_ID=${TOBOR_CLIENT_ID}
+TOBOR_REFRESH_TOKEN=${TOBOR_REFRESH_TOKEN}
+TOBOR_USER_TOKEN=${TOBOR_USER_TOKEN}
+EOT
+    export TEST_CONTAINER=$(docker run ${DOCKER_FLAGS} --env-file env  ${DOCKER_REPO}/${NAME}:${VERSION})
+fi;
 echo ${TEST_CONTAINER} >/tmp/${NAME}
+
 for i in {1..10};
 	do sleep 1
 	echo sleeping for 10s while container starts $i
